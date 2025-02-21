c172bc2a31be1585888d420cb0d0f2b75cd7a931
Antonio Dias <antonioisaacvd@gmail.com>
2025-02-21 12:00:05 -0300

    feat: estrutura básica do flask-login

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_login import LoginManager
 from database import db
 from config import Config
+from models.user import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 if __name__ == '__main__':
     app.run(debug=True)

d137a96148b0d8daba79bef317ecbac8dec47305
Antonio Dias <antonioisaacvd@gmail.com>
2025-02-21 13:35:58 -0300

    feat: view home

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask_login import LoginManager
 from app.database import db
 from app.config import Config
 from app.models.user import User 
+from app.routes.views.home import home_bp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.config.from_object(Config)
 
 db.init_app(app)
@@ -12,6 +13,8 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
+
+app.register_blueprint(home_bp)
 
 @login_manager.user_loader
 def load_user(user_id):
